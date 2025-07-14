ddf78017d8be2c50b7a4fb4badc99c95d9e98719
Malte Tashiro <malte@iij.ad.jp>
2025-07-14 20:26:37 +0900

    First version

---
+++ api/rpki-history-api.py
@@ -0,0 +1,159 @@
+import ipaddress
+import os
+from datetime import datetime, timezone
+
+import falcon
+import psycopg
+
+db_host = os.environ['POSTGRES_HOST']
+db_dbname = os.environ['POSTGRES_DB']
+db_user = os.environ['POSTGRES_USER']
+with open('/run/secrets/postgres-pw', 'r') as f:
+    db_password = f.read()
+
+# These are the column names which should be retrieved from the database.
+vrp_dict_fields = ['prefix', 'asn', 'max_length', 'trust_anchor', 'visible']
+
+
+def rows_to_vrp(c: psycopg.Cursor) -> list:
+    """Convert the rows from the current result set of the cursor to a list of VRPs.
+
+    Each VRP is in a dictionary containing fields specified by vrp_dict_fields.
+    """
+    if c.description is None:
+        return list()
+    # To be robust against changes in the database schema and/or reordering of columns,
+    # use a index map.
+    cn_idx = {column.name: idx for idx, column in enumerate(c.description)}
+    ret = [{k: row[cn_idx[k]] for k in vrp_dict_fields} for row in c.fetchall()]
+    return ret
+
+
+def get_covering_vrps_for_prefix_at_time(c: psycopg.Cursor, prefix, timestamp: datetime) -> list:
+    c.execute("""
+        SELECT * FROM vrps
+        WHERE prefix >>= %s
+        AND visible @> %s
+    """, (prefix, timestamp))
+    return rows_to_vrp(c)
+
+
+def get_rpki_status(c: psycopg.Cursor, prefix, timestamp: datetime, asn: int) -> dict:
+    """Infer the RPKI status for the specified prefix/origin ASN combination at the
+    specified timestamp.
+
+    Return the result as a dictionary, ready for serialization.
+
+    See: https://www.rfc-editor.org/rfc/rfc6811#section-2.1
+    """
+    vrps = get_covering_vrps_for_prefix_at_time(c, prefix, timestamp)
+    if not vrps:
+        return {'status': 'NotFound'}
+    for vrp in vrps:
+        if (prefix.prefixlen <= vrp['max_length']
+            and vrp['asn'] != 0
+                and vrp['asn'] == asn):
+            return {'status': 'Valid'}
+    return {'status': 'Invalid'}
+
+
+def get_latest_dump_ts(c: psycopg.Cursor) -> datetime | None:
+    """Get the latest dump time as datetime from the database."""
+    c.execute('SELECT dump_time FROM metadata ORDER BY dump_time DESC LIMIT 1')
+    res = c.fetchone()
+    if res is None:
+        return None
+    return res[0]
+
+
+def parse_timestamp(timestamp: str) -> datetime:
+    """Parse a timestamp either in %Y-%m-%dT%H:%M:%S or unix epoch format and return the
+    corresponding datetime.
+    """
+    try:
+        return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S').replace(tzinfo=timezone.utc)
+    except ValueError:
+        pass
+    try:
+        return datetime.fromtimestamp(int(timestamp), tz=timezone.utc)
+    except ValueError:
+        raise falcon.HTTPInvalidParam('Timestamp has to be in epoch or %Y-%m-%dT%H:%M:%S format.', 'timestamp')
+
+
+class VRPResource:
+    def __init__(self) -> None:
+        self.conn = psycopg.connect(
+            host=db_host,
+            dbname=db_dbname,
+            user=db_user,
+            password=db_password
+        )
+
+    def on_get(self, req: falcon.Request, resp: falcon.Response):
+        """Return all covering VRPs for the requested prefix at the specified timestamp.
+
+        If no timestamp is specified, return data for the latest available dump in the
+        database.
+        """
+        if not req.has_param('prefix'):
+            raise falcon.HTTPMissingParam('prefix')
+
+        try:
+            parsed_prefix = ipaddress.ip_network(req.get_param('prefix', required=True))
+        except ValueError as e:
+            raise falcon.HTTPInvalidParam(str(e), 'prefix')
+
+        with self.conn.cursor() as c:
+            if req.has_param('timestamp'):
+                timestamp = parse_timestamp(req.get_param('timestamp', required=True))
+            else:
+                timestamp = get_latest_dump_ts(c)
+                if timestamp is None:
+                    raise falcon.HTTPInternalServerError(description='Failed to get latest dump time.')
+
+            vrps = get_covering_vrps_for_prefix_at_time(c, parsed_prefix, timestamp)
+            # Format for JSON serialization.
+            for vrp in vrps:
+                vrp['prefix'] = vrp['prefix'].compressed
+                vrp['visible'] = {'from': vrp['visible'].lower.isoformat(),
+                                  'to': vrp['visible'].upper.isoformat()}
+            resp.media = vrps
+
+
+class StatusResource:
+    def __init__(self) -> None:
+        self.conn = psycopg.connect(
+            host=db_host,
+            dbname=db_dbname,
+            user=db_user,
+            password=db_password
+        )
+
+    def on_get(self, req: falcon.Request, resp: falcon.Response):
+        """Return the RPKI status for the specified prefix and originating ASN."""
+        required_params = ['prefix', 'asn']
+        for param in required_params:
+            if not req.has_param(param):
+                raise falcon.HTTPMissingParam(param)
+
+        try:
+            parsed_prefix = ipaddress.ip_network(req.get_param('prefix', required=True))
+        except ValueError as e:
+            raise falcon.HTTPInvalidParam(str(e), 'prefix')
+
+        asn = req.get_param_as_int('asn', required=True)
+
+        with self.conn.cursor() as c:
+            if req.has_param('timestamp'):
+                timestamp = parse_timestamp(req.get_param('timestamp', required=True))
+            else:
+                timestamp = get_latest_dump_ts(c)
+                if timestamp is None:
+                    raise falcon.HTTPInternalServerError(description='Failed to get latest dump time.')
+
+            resp.media = get_rpki_status(c, parsed_prefix, timestamp, asn)
+
+
+application = falcon.App()
+application.add_route('/vrp', VRPResource())
+application.add_route('/status', StatusResource())
---
+++ db_scripts/rpki-history-db.py
@@ -0,0 +1,259 @@
+import argparse
+import csv
+import ipaddress
+import logging
+import os
+import subprocess as sp
+import sys
+from collections import namedtuple
+from datetime import datetime, timedelta, timezone
+
+import psycopg
+import requests
+from bs4 import BeautifulSoup
+from psycopg.types.range import Range
+
+vrp_tuple_fields = ['prefix', 'asn', 'max_length', 'trust_anchor']
+VRP = namedtuple('VRP', vrp_tuple_fields)
+
+URL_FMT = 'https://dango.attn.jp/rpkidata/%Y/%m/%d/'
+FILE_FMT = 'rpki-%Y%m%dT%H%M%SZ.tgz'
+
+
+class RPKIHistory:
+    def __init__(self) -> None:
+        self.db_host = os.environ['POSTGRES_HOST']
+        self.db_dbname = os.environ['POSTGRES_DB']
+        self.db_user = os.environ['POSTGRES_USER']
+        with open('/run/secrets/postgres-pw', 'r') as f:
+            self.db_password = f.read()
+        self.new_file_url = str()
+        self.new_file_content = bytes()
+        self.new_ts = None
+        self.new_vrps = set()
+        self.latest_ts = None
+        self.latest_vrps = dict()
+
+    def __enter__(self):
+        self.conn = psycopg.connect(
+            host=self.db_host,
+            dbname=self.db_dbname,
+            user=self.db_user,
+            password=self.db_password
+        )
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            self.conn.rollback()
+        self.conn.close()
+
+    @staticmethod
+    def get_datetime_from_filename(fname: str) -> datetime:
+        return datetime.strptime(fname, FILE_FMT).replace(tzinfo=timezone.utc)
+
+    def get_new_file_url(self) -> None:
+        """Get the URL of the newest available file."""
+        curr_ts = datetime.now(tz=timezone.utc)
+        folder_url = curr_ts.strftime(URL_FMT)
+        r = requests.get(folder_url)
+        # If we are just after midnight UTC, we should look at the previous day.
+        if r.status_code == 404:
+            curr_ts -= timedelta(hours=24)
+            folder_url = curr_ts.strftime(URL_FMT)
+            r = requests.get(folder_url)
+            try:
+                r.raise_for_status()
+            except Exception as e:
+                logging.error(f'Failed to get latest file: {e}')
+                return
+
+        soup = BeautifulSoup(r.text, features='html.parser')
+        new_file = str()
+        for link in soup.find_all('a'):
+            href = link['href']
+            if not href.startswith('rpki-'):
+                continue
+
+            file_ts = RPKIHistory.get_datetime_from_filename(href)
+            if self.new_ts is None or file_ts > self.new_ts:
+                self.new_ts = file_ts
+                new_file = href
+
+        if not new_file:
+            logging.error(f'Failed to find valid file in folder: {folder_url}')
+            return
+
+        self.new_file_url = os.path.join(folder_url, new_file)
+
+    def read_file(self) -> None:
+        """Read the contents of the downloaded file and parse VRP entries."""
+        logging.info('Reading file')
+        base = os.path.basename(self.new_file_url).removesuffix('.tgz')
+        member = f'{base}/output/rpki-client.csv'
+        ps = sp.run(['tar', '--gunzip', '--extract', '--to-stdout', member],
+                    input=self.new_file_content,
+                    stdout=sp.PIPE,
+                    check=True)
+
+        for l in csv.DictReader(ps.stdout.decode().splitlines()):
+            asn = int(l['ASN'].removeprefix('AS'))
+            prefix = ipaddress.ip_network(l['IP Prefix'])
+            trust_anchor = l['Trust Anchor']
+            max_length = int(l['Max Length'])
+            self.new_vrps.add(VRP(prefix, asn, max_length, trust_anchor))
+        logging.info(f'Read {len(self.new_vrps)} unique VRPs from file')
+
+    def fetch_and_read_file(self) -> None:
+        """Download the file specified by new_file_url and parse its contents."""
+        logging.info(f'Fetching file: {self.new_file_url}')
+        r = requests.get(self.new_file_url)
+        r.raise_for_status()
+        self.new_file_content = r.content
+        self.read_file()
+
+    def fetch_and_read_new_file(self) -> bool:
+        """Find the newest available file and process it if it is not already in the
+        database.
+
+        Return True if new data is available, False otherwise.
+        """
+        self.get_new_file_url()
+        if not self.new_file_url or self.new_ts <= self.latest_ts:
+            return False
+        self.fetch_and_read_file()
+        return True
+
+    def fetch_and_read_specific_file(self, ts: datetime) -> None:
+        self.new_ts = ts
+        self.new_file_url = os.path.join(
+            ts.strftime(URL_FMT),
+            ts.strftime(FILE_FMT)
+        )
+        self.fetch_and_read_file()
+
+    def init_db(self):
+        with self.conn.cursor() as c:
+            c.execute("""
+            CREATE TABLE IF NOT EXISTS vrps (
+                id bigserial PRIMARY KEY,
+                prefix cidr,
+                asn bigint,
+                max_length integer,
+                trust_anchor text,
+                visible tstzrange)
+            """)
+            c.execute("""
+            CREATE TABLE IF NOT EXISTS metadata (
+                id serial PRIMARY KEY,
+                dump_time timestamp (0) with time zone,
+                ingest_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP)
+            """)
+
+    def get_latest_dump_ts(self, c: psycopg.Cursor) -> None:
+        c.execute('SELECT dump_time FROM metadata ORDER BY dump_time DESC LIMIT 1')
+        res = c.fetchone()
+        if res is not None:
+            self.latest_ts = res[0]
+        logging.info(f'Latest dump timestamp: {self.latest_ts}')
+
+    def rows_to_vrp(self, c: psycopg.Cursor) -> dict:
+        if c.description is None:
+            return dict()
+        cn_idx = {column.name: idx for idx, column in enumerate(c.description)}
+        return {
+            VRP(
+                *[e[cn_idx[column_name]]
+                  for column_name in vrp_tuple_fields]
+            ):
+            (e[cn_idx['id']], e[cn_idx['visible']])
+            for e in c.fetchall()
+        }
+
+    def get_latest_vrps(self, c: psycopg.Cursor) -> None:
+        if self.latest_ts is None:
+            return
+
+        c.execute("""
+            SELECT * FROM vrps
+            WHERE visible @> %s
+        """, (self.latest_ts, ))
+        self.latest_vrps = self.rows_to_vrp(c)
+        logging.info(f'Loaded {len(self.latest_vrps)} VRPs from database')
+
+    def update_db(self, timestamp: datetime = None):
+        with self.conn.cursor() as c:
+            self.get_latest_dump_ts(c)
+
+            if timestamp:
+                self.fetch_and_read_specific_file(timestamp)
+            else:
+                if not self.fetch_and_read_new_file():
+                    logging.info('No new data available.')
+                    return
+            self.get_latest_vrps(c)
+
+            c.execute('INSERT INTO metadata (dump_time) VALUES (%s)', (self.new_ts, ))
+            update_data = list()
+            update_vrps = self.new_vrps.intersection(self.latest_vrps.keys())
+            for vrp in update_vrps:
+                vrp_id, visible_range = self.latest_vrps[vrp]
+                update_data.append((Range(visible_range.lower, self.new_ts, bounds='[]'), vrp_id))
+            logging.info(f'Updating {len(update_data)} VRPs')
+            c.executemany("""
+                UPDATE vrps
+                SET visible = %s
+                WHERE id = %s
+            """, update_data)
+
+            insert_data = [
+                (vrp.prefix, vrp.asn, vrp.max_length, vrp.trust_anchor, Range(self.new_ts, self.new_ts, bounds='[]'))
+                for vrp in self.new_vrps - self.latest_vrps.keys()
+            ]
+            logging.info(f'Inserting {len(insert_data)} new VRPs')
+            c.executemany("""
+                INSERT INTO vrps (prefix, asn, max_length, trust_anchor, visible)
+                VALUES (%s, %s, %s, %s, %s)
+            """, insert_data)
+            self.conn.commit()
+
+
+if __name__ == '__main__':
+    handlers = [
+        logging.StreamHandler(sys.stdout)
+    ]
+    if os.path.exists('/log'):
+        handlers.append(logging.FileHandler('/log/rpki-history.log'))
+    FORMAT = '%(asctime)s %(levelname)s %(message)s'
+    logging.basicConfig(
+        format=FORMAT,
+        level=logging.INFO,
+        handlers=handlers,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info(f'Started: {sys.argv}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command')
+    parser.add_argument('-t', '--timestamp', help='fetch file for specific timestamp (YYYYMMDDThh:mm:ss)')
+    args = parser.parse_args()
+    rpki = RPKIHistory()
+    command = args.command
+    match command:
+        case 'init':
+            with rpki:
+                rpki.init_db()
+        case 'update':
+            timestamp = None
+            if args.timestamp:
+                try:
+                    timestamp = datetime.strptime(args.timestamp, '%Y%m%dT%H%M%S').replace(tzinfo=timezone.utc)
+                except ValueError as e:
+                    logging.error(f'Invalid timestamp specified: {args.timestamp} ({e})')
+                    sys.exit(1)
+            with rpki:
+                rpki.update_db(timestamp)
+        case _:
+            logging.error(f'Invalid command specified: {command}')
+            sys.exit(1)
+    logging.info('Done')
+    sys.exit(0)
